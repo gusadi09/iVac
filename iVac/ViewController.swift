e8f452e20db5deafc0351a19a1450d42ca4f8217
gusadi <gusadi872@gmail.com>
2021-01-28 09:45:09 +0800

    Initial Commit

---
+++ iVac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iVac
+//
+//  Created by Gus Adi on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
